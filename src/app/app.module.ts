9aaf08a559e5616afdf6ee79bd13d8fc3574bb92
gopigoppu <gopi.learner93@gmail.com>
2018-03-20 08:02:43 +0530

    components generated for login, search, header and contributors page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SearchComponent } from './components/search/search.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContributorsComponent } from './components/contributors/contributors.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SearchComponent,
+    HeaderComponent,
+    ContributorsComponent
   ],
   imports: [
     BrowserModule,
